b10265ae1cb37a8fedbd1b2aa4124ca370a8b704
= <=>
2022-11-17 18:09:38 +0530

    Added Files

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -6,12 +6,12 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should not have post without title" do
+  test 'should not have post without title' do
     post = Post.new
-    assert_not post.save, "Save the post withpout a title"
+    assert_not post.save, 'Save the post withpout a title'
   end
-  test "Should save post with title" do
-  post = Post.new(title:"Test")
-  assert post.save, "Idk words go here"
+  test 'Should save post with title' do
+  post = Post.new(title:'Test')
+  assert post.save, 'Idk words go here'
   end
 end
