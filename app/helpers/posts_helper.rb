b2c5c425d161496a399b411e191db4f503f75a78
= <=>
2022-11-17 17:33:25 +0530

    added Test File

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# This is Concerned with helping us
+module PostsHelper
+end
---
+++ test/models/post_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "should not have post without title" do
+    post = Post.new
+    assert_not post.save, "Save the post withpout a title"
+  end
+  test "Should save post with title" do
+  post = Post.new(title: "Test")
+  assert post.save, "Idk words go here"
+  end
+end
